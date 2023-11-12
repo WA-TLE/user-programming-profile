164e8201e3011a839c77a8494cff165f94ef9507
dy <dyu32345@gmail.com>
2023-11-12 20:36:15 +0800

    用户中心初始项目

---
+++ src/main/java/com/dy/UserProgrammingProfileApplication.java
@@ -0,0 +1,13 @@
+package com.dy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProgrammingProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProgrammingProfileApplication.class, args);
+    }
+
+}
